uncletrunks/OXCE-od

commit 0c2306e581b2b6f04b69e871f9fab9592c65ab63

Added "allowAlienBasesOnWrongTextures" global switch

src/Savegame/AlienMission.cpp
@@ -301,7 +301,10 @@ void AlienMission::think(Game &engine, const Globe &globe)
 					area.latMin = pos.second;
 					area.latMax = pos.second;
 				}
-				spawnAlienBase((*c), engine, area, pos, 0);
+				if (tries < 100 || mod.getAllowAlienBasesOnWrongTextures())
+				{
+					spawnAlienBase((*c), engine, area, pos, 0);
+				}
 				break;
 			}
 		}
@@ -346,7 +349,10 @@ void AlienMission::think(Game &engine, const Globe &globe)
 				}
 			}
 		}
-		spawnAlienBase(0, engine, area, pos, 0);
+		if (tries < 100 || mod.getAllowAlienBasesOnWrongTextures())
+		{
+			spawnAlienBase(0, engine, area, pos, 0);
+		}
 	}
 
 	if (_nextWave != _rule.getWaveCount())
@@ -657,14 +663,17 @@ void AlienMission::start(Game &engine, const Globe &globe, size_t initialCount)
 						}
 					}
 				}
-				auto newAlienOperationBase = spawnAlienBase(0, engine, area, pos, operationBaseType);
+				AlienBase* newAlienOperationBase = nullptr;
+				if (tries < 100 || mod.getAllowAlienBasesOnWrongTextures())
+				{
+					spawnAlienBase(0, engine, area, pos, operationBaseType);
+				}
 				if (newAlienOperationBase)
 				{
 					_base = newAlienOperationBase;
 				}
 				else
 				{
-					// can't happen, but I'll leave it here just in case alien base spawning gets changed in the future
 					_interrupted = true;
 				}
 			}

src/Mod/Mod.cpp
@@ -329,7 +329,8 @@ Mod::Mod() :
 	_aiFireChoiceIntelCoeff(5), _aiFireChoiceAggroCoeff(5), _aiExtendedFireModeChoice(false), _aiRespectMaxRange(false), _aiDestroyBaseFacilities(false),
 	_aiPickUpWeaponsMoreActively(false),
 	_maxLookVariant(0), _tooMuchSmokeThreshold(10), _customTrainingFactor(100), _minReactionAccuracy(0), _chanceToStopRetaliation(0), _lessAliensDuringBaseDefense(false),
-	_allowCountriesToCancelAlienPact(false), _buildInfiltrationBaseCloseToTheCountry(false), _kneelBonusGlobal(115), _oneHandedPenaltyGlobal(80),
+	_allowCountriesToCancelAlienPact(false), _buildInfiltrationBaseCloseToTheCountry(false), _allowAlienBasesOnWrongTextures(true),
+	_kneelBonusGlobal(115), _oneHandedPenaltyGlobal(80),
 	_enableCloseQuartersCombat(0), _closeQuartersAccuracyGlobal(100), _closeQuartersTuCostGlobal(12), _closeQuartersEnergyCostGlobal(8),
 	_noLOSAccuracyPenaltyGlobal(-1),
 	_surrenderMode(0),
@@ -1971,6 +1972,7 @@ void Mod::loadFile(const FileMap::FileRecord &filerec, ModScript &parsers)
 	_lessAliensDuringBaseDefense = doc["lessAliensDuringBaseDefense"].as<bool>(_lessAliensDuringBaseDefense);
 	_allowCountriesToCancelAlienPact = doc["allowCountriesToCancelAlienPact"].as<bool>(_allowCountriesToCancelAlienPact);
 	_buildInfiltrationBaseCloseToTheCountry = doc["buildInfiltrationBaseCloseToTheCountry"].as<bool>(_buildInfiltrationBaseCloseToTheCountry);
+	_allowAlienBasesOnWrongTextures = doc["allowAlienBasesOnWrongTextures"].as<bool>(_allowAlienBasesOnWrongTextures);
 	_kneelBonusGlobal = doc["kneelBonusGlobal"].as<int>(_kneelBonusGlobal);
 	_oneHandedPenaltyGlobal = doc["oneHandedPenaltyGlobal"].as<int>(_oneHandedPenaltyGlobal);
 	_enableCloseQuartersCombat = doc["enableCloseQuartersCombat"].as<int>(_enableCloseQuartersCombat);

src/Mod/Mod.h
@@ -200,6 +200,7 @@ private:
 	int _chanceToStopRetaliation;
 	bool _lessAliensDuringBaseDefense;
 	bool _allowCountriesToCancelAlienPact, _buildInfiltrationBaseCloseToTheCountry;
+	bool _allowAlienBasesOnWrongTextures;
 	int _kneelBonusGlobal, _oneHandedPenaltyGlobal;
 	int _enableCloseQuartersCombat, _closeQuartersAccuracyGlobal, _closeQuartersTuCostGlobal, _closeQuartersEnergyCostGlobal;
 	int _noLOSAccuracyPenaltyGlobal;
@@ -571,6 +572,8 @@ public:
 	bool getAllowCountriesToCancelAlienPact() const { return _allowCountriesToCancelAlienPact; }
 	/// Should alien infiltration bases be built close to the infiltrated country?
 	bool getBuildInfiltrationBaseCloseToTheCountry() const { return _buildInfiltrationBaseCloseToTheCountry; }
+	/// Should alien bases be allowed (in worst case) on invalid globe textures or not?
+	bool getAllowAlienBasesOnWrongTextures() const { return _allowAlienBasesOnWrongTextures; }
 	/// Gets the global kneel bonus (default = 115).
 	int getKneelBonusGlobal() const { return _kneelBonusGlobal; }
 	/// Gets the global one-handed penalty (default = 80).

narical/openxcom-accuracy

commit fa8eb02f29e22605a314d494080a9d2fcd7db84d

Fixed copypaste error
introduced in 0c2306e581b2b6f04b69e871f9fab9592c65ab63

src/Savegame/AlienMission.cpp
@@ -678,7 +678,7 @@ void AlienMission::start(Game &engine, const Globe &globe, size_t initialCount)
 				AlienBase* newAlienOperationBase = nullptr;
 				if (tries < 100 || mod.getAllowAlienBasesOnWrongTextures())
 				{
-					spawnAlienBase(0, engine, pos, operationBaseType);
+					newAlienOperationBase = spawnAlienBase(0, engine, pos, operationBaseType);
 				}
 				if (newAlienOperationBase)
 				{