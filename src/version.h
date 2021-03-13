08d9eb908265b1fed482ff388d1ea7e8102d758f
Meridian <meridian@centrum.sk>
2021-03-13 08:01:31 +0100

    OXCE v7.0

--- src/version.h
+++ src/version.h
@@ -18,10 +18,10 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "Extended 6.9.7"
-#define OPENXCOM_VERSION_LONG "6.9.7.0"
-#define OPENXCOM_VERSION_NUMBER 6,9,7,0
+#define OPENXCOM_VERSION_SHORT "Extended 7.0"
+#define OPENXCOM_VERSION_LONG "7.0.0.0"
+#define OPENXCOM_VERSION_NUMBER 7,0,0,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2021-02-27)"
+#define OPENXCOM_VERSION_GIT " (v2021-03-13)"
 #endif
