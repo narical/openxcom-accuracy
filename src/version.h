ee7a65a70d88636ed853d5d524d065c5957dadcb
Yankes <Yankes@users.noreply.github.com>
2021-05-07 22:00:51 +0200

    OXCE 7.0.6

--- src/version.h
+++ src/version.h
@@ -18,10 +18,10 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "Extended 7.0.5"
-#define OPENXCOM_VERSION_LONG "7.0.5.0"
-#define OPENXCOM_VERSION_NUMBER 7,0,5,0
+#define OPENXCOM_VERSION_SHORT "Extended 7.0.6"
+#define OPENXCOM_VERSION_LONG "7.0.6.0"
+#define OPENXCOM_VERSION_NUMBER 7,0,6,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2021-05-02)"
+#define OPENXCOM_VERSION_GIT " (v2021-05-07)"
 #endif
