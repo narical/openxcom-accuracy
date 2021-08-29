56c475a901bfb99628f7bd2bca835f3d4b2c0913
Yankes <Yankes@users.noreply.github.com>
2021-08-29 14:55:19 +0200

    Fix bug in propagation of skill for psi-amp

--- src/Battlescape/PsiAttackBState.cpp
+++ src/Battlescape/PsiAttackBState.cpp
@@ -104,7 +104,7 @@ void PsiAttackBState::init()
 
 	int height = _target->getFloatHeight() + (_target->getHeight() / 2) - _parent->getSave()->getTile(_action.target)->getTerrainLevel();
 	Position voxel = _action.target.toVoxel() + Position(8, 8, height);
-	_parent->statePushFront(new ExplosionBState(_parent, voxel, BattleActionAttack{ _action.type, _action.actor, _action.weapon, _action.weapon }));
+	_parent->statePushFront(new ExplosionBState(_parent, voxel, BattleActionAttack::GetAferShoot(_action, _action.weapon)));
 }
 
 
