4fa79ed41c081d1142f581a394d3e2f82ae65bce
Meridian <meridian@centrum.sk>
2022-01-04 12:51:21 +0100

    OXCE 7.3.3

--- src/version.h
+++ src/version.h
@@ -18,10 +18,10 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "Extended 7.3.2"
-#define OPENXCOM_VERSION_LONG "7.3.2.0"
-#define OPENXCOM_VERSION_NUMBER 7,3,2,0
+#define OPENXCOM_VERSION_SHORT "Extended 7.3.3"
+#define OPENXCOM_VERSION_LONG "7.3.3.0"
+#define OPENXCOM_VERSION_NUMBER 7,3,3,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2022-01-02)"
+#define OPENXCOM_VERSION_GIT " (v2022-01-04)"
 #endif
