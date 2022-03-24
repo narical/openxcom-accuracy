8370fe83560c52e35d9df178260317662a14b84e
Yankes <Yankes@users.noreply.github.com>
2022-03-24 21:21:21 +0100

    Make voxel distance of trajectory more accurate

--- src/Battlescape/Projectile.cpp
+++ src/Battlescape/Projectile.cpp
@@ -456,6 +456,11 @@ bool Projectile::move()
 			_position--;
 			return false;
 		}
+		else if (_position > 1)
+		{
+			// calc avg of two voxel steps
+			_distance += 0.5f * Position::distance(_trajectory[_position], _trajectory[_position - 2]);
+		}
 		else if (_position > 0)
 		{
 			_distance += Position::distance(_trajectory[_position], _trajectory[_position - 1]);
