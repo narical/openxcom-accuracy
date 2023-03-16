03140e5d723028d60f0a24b483edff3731c9433c
Xilmi <ail.st@gmx.de>
2023-03-16 02:25:48 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -19,10 +19,10 @@
  */
 
 #define OPENXCOM_VERSION_ENGINE "Extended"
-#define OPENXCOM_VERSION_SHORT "Extended 7.8.12 + Brutal AI 3.8.1"
+#define OPENXCOM_VERSION_SHORT "Extended 7.8.12 + Brutal AI 3.9.0"
 #define OPENXCOM_VERSION_LONG "7.8.12.0"
 #define OPENXCOM_VERSION_NUMBER 7,8,12,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2023-03-13)"
+#define OPENXCOM_VERSION_GIT " (v2023-03-16)"
 #endif
