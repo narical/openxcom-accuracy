beb212353741d51cd8a682d6415bcf5c4987cd47
Xilmi <ail.st@gmx.de>
2023-05-30 01:53:01 +0200

    Update version.h

--- src/version.h
+++ src/version.h
@@ -19,7 +19,7 @@
  */
 
 #define OPENXCOM_VERSION_ENGINE "Extended"
-#define OPENXCOM_VERSION_SHORT "Extended Brutal 7.9.6 6.2.1"
+#define OPENXCOM_VERSION_SHORT "Extended Brutal 7.9.6 6.2.2"
 #define OPENXCOM_VERSION_LONG "7.9.6.0"
 #define OPENXCOM_VERSION_NUMBER 7,9,6,0
 
