uncletrunks/OXCE-od

commit ec83ddfe8f51882817a22b81ca440bc8e53c2bf4

Refactor of tile handling

src/Battlescape/Projectile.cpp
@@ -227,11 +227,9 @@ int Projectile::calculateThrow(double accuracy)
 	{
 		targets.push_back(targetVoxel);
 	}
-	else 
+	else
 	{
-		BattleUnit *tu = targetTile->getUnit();
-		if (!tu && _action.target.z > 0 && targetTile->hasNoFloor(0))
-			tu = _save->getTile(_action.target - Position(0, 0, 1))->getUnit();
+		BattleUnit *tu = targetTile->getOverlappingUnit(_save);
 		if (Options::forceFire && (SDL_GetModState() & KMOD_CTRL) != 0 && _save->getSide() == FACTION_PLAYER)
 		{
 			targets.push_back(_action.target * Position(16,16,24) + Position(0, 0, 12));
@@ -315,7 +313,7 @@ int Projectile::calculateThrow(double accuracy)
 			&& endTile->getMapData(O_OBJECT)->getTUCost(MT_WALK) == 255
 			&& !(endTile->isBigWall() && (endTile->getMapData(O_OBJECT)->getBigWall()<1 || endTile->getMapData(O_OBJECT)->getBigWall()>3)))
 		{
-			test = V_OUTOFBOUNDS; 
+			test = V_OUTOFBOUNDS;
 		}
 	}
 	return test;
@@ -390,7 +388,7 @@ void Projectile::applyAccuracy(Position origin, Position *target, double accurac
 		{
 			bool hasLOS = false;
 			BattleUnit *bu = _action.actor;
-			BattleUnit *targetUnit = t->getUnit();
+			BattleUnit *targetUnit = t->getOverlappingUnit(_save);
 
 			if (targetUnit)
 			{

src/Battlescape/AIModule.cpp
@@ -1876,7 +1876,7 @@ int AIModule::explosiveEfficacy(Position targetPos, BattleUnit *attackingUnit, i
 	Tile *targetTile = _save->getTile(targetPos);
 
 	// don't throw grenades at flying enemies.
-	if (grenade && targetPos.z > 0 && targetTile->hasNoFloor(_save->getTile(targetPos - Position(0,0,1))))
+	if (grenade && targetPos.z > 0 && targetTile->hasNoFloor(_save))
 	{
 		return false;
 	}
@@ -2137,7 +2137,7 @@ void AIModule::wayPointAction()
 			}
 			else if (CollidesWith == V_UNIT)
 			{
-				BattleUnit* target = _save->getTile(CurrentPosition)->getUnit();
+				BattleUnit* target = _save->getTile(CurrentPosition)->getOverlappingUnit(_save);
 				if (target == _aggroTarget)
 				{
 					_attackAction->waypoints.push_back(CurrentPosition);

src/Battlescape/BattlescapeGame.cpp
@@ -1991,7 +1991,7 @@ BattleUnit *BattlescapeGame::convertUnit(BattleUnit *unit)
 	unit->setTile(0);
 
 	Unit* type = getMod()->getUnit(unit->getSpawnUnit(), true);
-	getSave()->getTile(unit->getPosition())->setUnit(0);
+	getSave()->getTile(unit->getPosition())->clearUnit();
 
 	BattleUnit *newUnit = new BattleUnit(type,
 		FACTION_HOSTILE,
@@ -2002,7 +2002,7 @@ BattleUnit *BattlescapeGame::convertUnit(BattleUnit *unit)
 		getMod()->getMaxViewDistance());
 
 	getSave()->initUnit(newUnit);
-	getSave()->getTile(unit->getPosition())->setUnit(newUnit, _save->getTile(unit->getPosition() + Position(0,0,-1)));
+	getSave()->getTile(unit->getPosition())->setUnit(newUnit, _save);
 	newUnit->setPosition(unit->getPosition());
 	newUnit->setDirection(unit->getDirection());
 	newUnit->setTimeUnits(0);
@@ -2122,7 +2122,7 @@ void BattlescapeGame::spawnNewUnit(BattleActionAttack attack, Position position)
 			for (int y = newUnit->getArmor()->getSize() - 1; y >= 0; y--)
 			{
 				Tile* tile = getSave()->getTile(position + Position(x, y, 0));
-				tile->setUnit(newUnit, getSave()->getTile(position + Position(x, y, -1)));
+				tile->setUnit(newUnit, getSave());
 			}
 		}
 		newUnit->setPosition(position);
@@ -2192,7 +2192,7 @@ void BattlescapeGame::removeSummonedPlayerUnits()
 				for (int y = (*unit)->getArmor()->getSize() - 1; y >= 0; y--)
 				{
 					Tile *tile = getSave()->getTile((*unit)->getPosition() + Position(x, y, 0));
-					tile->setUnit(0);
+					tile->clearUnit();
 				}
 			}
 

src/Battlescape/BattlescapeGenerator.cpp
@@ -256,7 +256,7 @@ void BattlescapeGenerator::nextStage()
 			{
 				for (int y = 0; y != size; ++y)
 				{
-					_save->getTile(pos + Position(x,y,0))->setUnit(0);
+					_save->getTile(pos + Position(x,y,0))->clearUnit();
 				}
 			}
 		}
@@ -476,7 +476,7 @@ void BattlescapeGenerator::nextStage()
 					{
 						_craftInventoryTile = (*j)->getTile();
 					}
-					_craftInventoryTile->setUnit(*j);
+					_craftInventoryTile->setUnit(*j, _save);
 					(*j)->setVisible(false);
 					if ((*j)->getId() > highestSoldierID)
 					{
@@ -503,7 +503,7 @@ void BattlescapeGenerator::nextStage()
 			_craftInventoryTile->addItem(*i, ground);
 			if ((*i)->getUnit())
 			{
-				_craftInventoryTile->setUnit((*i)->getUnit());
+				_craftInventoryTile->setUnit((*i)->getUnit(), _save);
 				(*i)->getUnit()->setPosition(_craftInventoryTile->getPosition());
 			}
 		}
@@ -800,7 +800,7 @@ void BattlescapeGenerator::deployXCOM(const RuleStartingCondition *startingCondi
 	{
 		if ((*i)->getFaction() == FACTION_PLAYER)
 		{
-			_craftInventoryTile->setUnit(*i);
+			_craftInventoryTile->setUnit(*i, _save);
 			(*i)->setVisible(false);
 		}
 	}

src/Battlescape/BattlescapeState.cpp
@@ -2254,7 +2254,7 @@ inline void BattlescapeState::handle(Action *action)
 							Tile *tile = _save->getTile(newPos);
 							if (tile)
 							{
-								BattleUnit *unit = tile->getUnit();
+								BattleUnit *unit = tile->getOverlappingUnit(_save);
 								if (unit && !unit->isOut())
 								{
 									debug("Bingo!");
@@ -2299,9 +2299,9 @@ inline void BattlescapeState::handle(Action *action)
 								debug("Beam me up Scotty");
 								_save->getPathfinding()->removePreview();
 
-								unit->getTile()->setUnit(0);
+								unit->getTile()->clearUnit();
 								unit->setPosition(newPos);
-								tile->setUnit(unit);
+								tile->setUnit(unit, _save);
 								_save->getTileEngine()->calculateLighting(LL_UNITS);
 								_save->getBattleGame()->handleState();
 							}
@@ -2422,7 +2422,7 @@ void BattlescapeState::saveAIMap()
 					break;
 				}
 				pos.z--;
-				if (z > 0 && !t->hasNoFloor(_save->getTile(pos))) break; // no seeing through floors
+				if (z > 0 && !t->hasNoFloor(_save)) break; // no seeing through floors
 			}
 
 			if (t->getMapData(O_NORTHWALL) && t->getMapData(O_NORTHWALL)->getTUCost(MT_FLY) == 255)

src/Battlescape/DebriefingState.cpp
@@ -1348,7 +1348,7 @@ void DebriefingState::prepareDebriefing()
 					}
 				}
 			}
-			(*j)->setTile(battle->getTile(pos));
+			(*j)->setTile(battle->getTile(pos), battle);
 		}
 
 		if (status == STATUS_DEAD)

src/Battlescape/ExplosionBState.cpp
@@ -110,11 +110,7 @@ void ExplosionBState::init()
 		if (type == BT_PSIAMP || _hit)
 		{
 			Position targetPos = _center.toTile();
-			_targetPsiOrHit = _parent->getSave()->getTile(targetPos)->getUnit();
-			if (!_targetPsiOrHit && targetPos.z > 0)
-			{
-				_targetPsiOrHit = _parent->getSave()->getTile(targetPos - Position(0, 0, 1))->getUnit();
-			}
+			_targetPsiOrHit = _parent->getSave()->getTile(targetPos)->getOverlappingUnit(_parent->getSave());
 		}
 
 		//testing if we hit target

src/Battlescape/Map.cpp
@@ -364,25 +364,19 @@ void Map::drawUnit(UnitSprite &unitSprite, Tile *unitTile, Tile *currTile, Posit
 	{
 		return;
 	}
-	BattleUnit* bu = unitTile->getUnit();
+	BattleUnit* bu = unitTile->getOverlappingUnit(_save, TUO_ALWAYS);
 	Position unitOffset;
 	bool unitFromBelow = false;
-	if (!bu)
+	if (bu)
 	{
-		Tile *below = _save->getTile(unitTile->getPosition() + Position(0,0,-1));
-		if (below && unitTile->hasNoFloor(below))
+		if (bu != unitTile->getUnit())
 		{
-			bu = below->getUnit();
-			if (!bu)
-			{
-				return;
-			}
 			unitFromBelow = true;
 		}
-		else
-		{
-			return;
-		}
+	}
+	else
+	{
+		return;
 	}
 
 	if (!(bu->getVisible() || _save->getDebugMode()))
@@ -411,8 +405,8 @@ void Map::drawUnit(UnitSprite &unitSprite, Tile *unitTile, Tile *currTile, Posit
 	}
 	else
 	{
-		Tile *top = _save->getTile(unitTile->getPosition() + Position(0, 0, +1));
-		if (top && top->hasNoFloor(unitTile))
+		const Tile *top = _save->getAboveTile(unitTile);
+		if (top && top->hasNoFloor(_save))
 		{
 			topMargin = -tileFoorHeight / 2;
 		}
@@ -1062,7 +1056,7 @@ void Map::drawTerrain(Surface *surface)
 					// Draw Path Preview
 					if (tile->getPreview() != -1 && tile->isDiscovered(0) && (_previewSetting & PATH_ARROWS))
 					{
-						if (itZ > 0 && tile->hasNoFloor(_save->getTile(tile->getPosition() + Position(0,0,-1))))
+						if (itZ > 0 && tile->hasNoFloor(_save))
 						{
 							tmpSurface = _game->getMod()->getSurfaceSet("Pathfinding")->getFrame(11);
 							if (tmpSurface)
@@ -1389,13 +1383,12 @@ void Map::drawTerrain(Surface *surface)
 						screenPosition.y > -_spriteHeight && screenPosition.y < surface->getHeight() + _spriteHeight )
 					{
 						tile = _save->getTile(mapPosition);
-						Tile *tileBelow = _save->getTile(mapPosition - Position(0,0,1));
 						if (!tile || !tile->isDiscovered(0) || tile->getPreview() == -1)
 							continue;
 						int adjustment = -tile->getTerrainLevel();
 						if (_previewSetting & PATH_ARROWS)
 						{
-							if (itZ > 0 && tile->hasNoFloor(tileBelow))
+							if (itZ > 0 && tile->hasNoFloor(_save))
 							{
 								tmpSurface = _game->getMod()->getSurfaceSet("Pathfinding")->getFrame(23);
 								if (tmpSurface)

narical/openxcom-accuracy

commit c60099271133078d30b64b158d1cbbd1fd254d6d

Fix noLOS penalty calculation
Caused by refactor in ec83ddfe8f51882817a22b81ca440bc8e53c2bf4

src/Battlescape/Projectile.cpp
@@ -391,7 +391,7 @@ void Projectile::applyAccuracy(Position origin, Position *target, double accurac
 		{
 			bool hasLOS = false;
 			BattleUnit *bu = _action.actor;
-			BattleUnit *targetUnit = t->getOverlappingUnit(_save);
+			BattleUnit *targetUnit = t->getUnit(); // we can call TileEngine::visible() only if the target unit is on the same tile
 
 			if (targetUnit)
 			{