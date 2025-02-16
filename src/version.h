bf1fb353283fa9b3b0d0b783272a202cb0664f04
Meridian <meridian@centrum.sk>
2025-02-16 10:27:22 +0100

    OXCE v8.1

--- src/version.h
+++ src/version.h
@@ -18,13 +18,13 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define MIN_REQUIRED_RULESET_VERSION_NUMBER 8,0,0,0
+#define MIN_REQUIRED_RULESET_VERSION_NUMBER 8,1,0,0
 
 #define OPENXCOM_VERSION_ENGINE "Extended"
-#define OPENXCOM_VERSION_SHORT "Extended 8.0.9"
-#define OPENXCOM_VERSION_LONG "8.0.9.0"
-#define OPENXCOM_VERSION_NUMBER 8,0,9,0
+#define OPENXCOM_VERSION_SHORT "Extended 8.1.0"
+#define OPENXCOM_VERSION_LONG "8.1.0.0"
+#define OPENXCOM_VERSION_NUMBER 8,1,0,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2025-02-14)"
+#define OPENXCOM_VERSION_GIT " (v2025-02-16)"
 #endif
